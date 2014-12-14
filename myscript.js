5c03b026efb7e3cc719d086aa4b299becced37e5
Kris Plunkett <kris.j.plunkett@icloud.com>
2014-12-14 17:38:18 -0600

    adds first bit of JS

---
+++ myscript.js
@@ -0,0 +1,20 @@
+/* 
+* Script that handles interactive elements of input list
+* adds and removes input
+*/
+var x = document.getElementById('inputList').innerHTML;
+console.log(x);
+
+// Input object constructor
+function input (number, label) {
+	this.number = number;
+	this.label = label;
+}
+
+// Array of inputs
+var inputsArray = [];
+
+// Add a new input
+function addInput () {
+	var 
+}
