ffa55353480890569f01ac4d3797f6a3d63a3e66
Kris Plunkett <kris.j.plunkett@icloud.com>
2014-12-14 19:04:08 -0600

    addInput function works

--- myscript.js
+++ myscript.js
@@ -2,19 +2,42 @@
 * Script that handles interactive elements of input list
 * adds and removes input
 */
-var x = document.getElementById('inputList').innerHTML;
-console.log(x);
+
+// Global variables
+var inputNumber = 1;	
+var inputTable = document.getElementById('inputList');
 
 // Input object constructor
-function input (number, label) {
+function input (number, label, mic) {
 	this.number = number;
 	this.label = label;
+	this.mic = mic;
 }
-
-// Array of inputs
-var inputsArray = [];
 
 // Add a new input
 function addInput () {
-	var 
+	// create a new input object with the input number, an empty label, and an empty mic/line
+	var newInput = new input(inputNumber, "", "");
+	
+	// create a new <tr> element in the inputList table
+	var row = inputTable.insertRow(inputNumber);
+	
+	// create a new <td> element for the channel number
+	var channel = row.insertCell(0);
+	// assigns channel number to that <td>
+	channel.innerHTML = inputNumber;
+	
+	// create a new <td> element for the label
+	var label = row.insertCell(1);
+	// assigns a label to that <td>
+	label.innerHTML = newInput.label; 
+
+	// create a new <td> element for the mic/line
+	var micLine = row.insertCell(2);
+	// assigns a mic/line to that <td>
+	micLine.innerHTML = newInput.mic;
+
+	//increment inputNumber by 1
+	inputNumber++;
 }
+
